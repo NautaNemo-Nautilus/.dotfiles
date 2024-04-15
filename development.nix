e0e3a27cec9fbf96439a574a159f27237f1632f0
NautaNemo-Nautilus <NautaNemo@proton.me>
2024-04-15 22:26:49 +0900

    add

--- development.nix
+++ development.nix
@@ -3,7 +3,11 @@
     # C
     gcc
     # Python 
-    python312 
+    (python312.withPackages (ppkgs: [
+      ppkgs.numpy
+      ppkgs.scipy
+      ppkgs.matplotlib
+    ])) 
     # Haskell
     ghc
     
