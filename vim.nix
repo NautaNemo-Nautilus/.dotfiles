da0e75f5974ce498e1534dc224814966a0f512bc
NautaNemo-Nautilus <NautaNemo@proton.me>
2024-04-19 21:12:36 +0900

    fix

--- vim.nix
+++ vim.nix
@@ -42,7 +42,7 @@
       colorscheme iceberg
 
       " Plugin-Airline setting
-      let g:airline_theme='minimalist'
+      "let g:airline_theme='minimalist' <- it's option
       let g:airline#extensions#tabline#enabled = 1
       let g:airline_powerline_fonts = 1
 
