817c54128b92e3a190f3665babdcebb67fef97d5
NautaNemo-Nautilus <NautaNemo@proton.me>
2024-05-21 00:26:36 +0900

    fix

--- zsh.nix
+++ zsh.nix
@@ -7,8 +7,12 @@
     syntaxHighlighting.enable = true;
     history.size = 10000;
     initExtra = ''
+      #Nix
       if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
           . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+      # End Nix
       '';
+      
   };
 }
